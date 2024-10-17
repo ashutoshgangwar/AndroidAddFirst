64998c6138cee23163667cdc16932d156d9b0fba
Ashutosh Gangwar <ashutoshgangwar1994@gmail.com>
2024-10-17 21:49:30 +0530

    Registration_from.jsx handle submit improved

---
+++ app/(tabs)/Pages/SuccessRegistration.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect} from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { useNavigation } from "expo-router";
+import { Colors } from "../../../constants/Colors";
+
+
+
+const RegistrationSuccess = () => {
+  
+const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+})
+    
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        {/* Success icon */}
+        <Image
+          source={require("./../../../assets/images/success.png")} // You can use a custom checkmark icon here
+          style={styles.icon}
+        />
+
+        {/* Success message */}
+        <Text style={styles.successText}>Registration successful!</Text>
+        <Text style={styles.descriptionText}>
+          You form is already submited, Your Application number is:
+        </Text>
+
+        {/* OK button */}
+        <TouchableOpacity style={styles.okButton} onPress={""}>
+          <Text style={styles.okButtonText}>OK</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  card: {
+    width: 350,
+    padding: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    alignItems: 'center',
+    elevation: 5, // Shadow for Android
+    shadowColor: '#000', // Shadow for iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  icon: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  successText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.PRIMERY,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  descriptionText: {
+    fontSize: 15,
+    color: Colors.PRIMERY,
+    textAlign: 'center',
+    marginBottom: 20,
+    fontWeight:"black"
+  },
+  okButton: {
+    backgroundColor: Colors.PRIMERY,
+    paddingVertical: 10,
+    paddingHorizontal: 80,
+    borderRadius: 25,
+  },
+  okButtonText: {
+    color: '#FFF',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default RegistrationSuccess;
